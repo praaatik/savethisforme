05fc05ba7a02ad96589f682d169402496d847506
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2023-03-05 20:25:37 +0530

    Add edit/delete collections feature

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,7 +1,11 @@
-import { ListItem, ListItemButton, ListItemText, Tooltip } from '@mui/material'
-import React from 'react'
+import { Button, ListItem, ListItemButton, ListItemText, TextField, Tooltip } from '@mui/material'
+import React, { useState } from 'react'
 import ICollection from '../utils/interfaces/ICollection.interface'
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useUpdateCollectionNameMutation } from '../store';
 
 interface Props {
     collectionToDeleteSet: (value: React.SetStateAction<number>) => void
@@ -10,21 +14,44 @@ interface Props {
 }
 
 export default function Collection({ collectionToDeleteSet, handleDialogClickOpen, collection }: Props) {
-    return (
-        <ListItem key={collection.collectionId} divider sx={{ maxWidth: "20rem", }} >
-            <ListItemButton sx={{ maxWidth: "10rem" }} >
-                <ListItemText primary={collection.collectionName} />
-            </ListItemButton>
-            <Tooltip title="Delete collection" placement="top">
-                <ListItemText className="opacity-60 hover:opacity-100 cursor-pointer flex justify-center" >
-                    <DeleteIcon onClick={() => {
-                        collectionToDeleteSet(collection.collectionId)
-                        handleDialogClickOpen()
-                    }}
-                        className="cursor-pointer"
-                    />
-                </ListItemText>
-            </Tooltip>
-        </ListItem>
-    )
+    const [isCollectionEdit, isCollectionEditSet] = useState(false);
+    const [collectionName, collectionNameSet] = useState(collection.collectionName)
+    const [updateCollectionName, _] = useUpdateCollectionNameMutation()
+
+    const handleOnUpdate = () => {
+        if (collectionName !== "") {
+            updateCollectionName({ collectionId: collection?.collectionId, updatedCollectionName: collectionName })
+            isCollectionEditSet(false)
+        }
+    }
+
+    return !isCollectionEdit ? <ListItem key={collection.collectionId} divider sx={{ maxWidth: "20rem" }} >
+        <ListItemButton sx={{ width: "10rem" }}>
+            <ListItemText primary={collectionName} />
+        </ListItemButton>
+        <Tooltip title="Edit collection name" placement="top">
+            <ListItemText className="opacity-60 hover:opacity-100 cursor-pointer flex justify-center">
+                <EditIcon className="cursor-pointer" onClick={() => isCollectionEditSet(true)} />
+            </ListItemText>
+        </Tooltip>
+    </ListItem > : <ListItem key={collection.collectionId} divider sx={{ maxWidth: "20rem", display: "flex", justifyContent: "space-evenly" }} >
+        <TextField value={collectionName} onChange={(event) => collectionNameSet(event.target.value)} />
+        <ListItemButton sx={{ display: "flex", justifyContent: "center", }} onClick={handleOnUpdate}>
+            <CheckIcon className="cursor-pointer" color='success' />
+        </ListItemButton>
+        <ListItemButton sx={{ display: "flex", justifyContent: "center" }}>
+            <ClearIcon className="cursor-pointer" color="error" onClick={() => {
+                isCollectionEditSet(false)
+            }} />
+        </ListItemButton>
+        <ListItemButton sx={{ display: "flex", justifyContent: "center" }}>
+            <DeleteIcon onClick={() => {
+                collectionToDeleteSet(collection.collectionId)
+                handleDialogClickOpen()
+            }}
+                className="cursor-pointer"
+            />
+        </ListItemButton>
+    </ListItem >
 }
+
