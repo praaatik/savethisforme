dc96686d9f121dad374adc28a8287b18bfbf5bbb
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2023-03-20 12:37:33 +0530

    Update delete icon component.

--- src/components/DeleteCollectionIcon.tsx
+++ src/components/DeleteCollectionIcon.tsx
@@ -1,21 +1,22 @@
 import { ListItemText, Tooltip } from '@mui/material'
-import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface Props {
     handleDialogClickOpen: () => void
 }
 
-export default function DeleteCollectionIcon() {
+export default function DeleteCollectionIcon({ handleDialogClickOpen }: Props) {
     return (
         <Tooltip title="Delete collection" placement="top">
-            <ListItemText className="opacity-60 hover:opacity-100 cursor-pointer flex justify-center" >
+            {/* <ListItemText className="opacity-60 hover:opacity-100 cursor-pointer flex justify-center" >
                 <DeleteIcon onClick={() => {
                     collectionToDeleteSet(collection.collectionId)
                     handleDialogClickOpen()
                 }}
                     className="cursor-pointer"
                 />
-            </ListItemText>
+            </ListItemText> */}
+            <></>
         </Tooltip>
     )
 }
