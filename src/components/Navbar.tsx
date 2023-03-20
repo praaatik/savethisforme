cb8edaaa345f9a107d35f7b188ddc3d00fba4fe1
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2023-03-20 12:20:07 +0530

    Update logo to redirect to home on click.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { AppBar, Button } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import Logout from "./Logout";
+import { useNavigate } from 'react-router-dom'
+import useUserData from "../hooks/useUserData";
 
 interface Props {
     handleDrawerToggle?: () => void
@@ -8,6 +10,14 @@ interface Props {
 }
 export default function Navbar({ handleDrawerToggle, isFull }: Props) {
     const drawerWidth = 330;
+    const navigate = useNavigate()
+    const { user } = useUserData()
+
+    const handleOnClick = () => {
+        if (user === null) {
+            navigate("/")
+        }
+    }
 
     return (
         <AppBar
@@ -27,7 +37,7 @@ export default function Navbar({ handleDrawerToggle, isFull }: Props) {
                 <MenuIcon onClick={handleDrawerToggle} fontSize="large" />
             </div>
             <div className=" flex items-center justify-between w-full px-5">
-                <div className=" flex items-center w-full text-black">Save this for me</div>
+                <div className="h-full text-xl flex items-center justify-center w-full sm:w-fit text-black cursor-pointer" onClick={handleOnClick}>Save this for me</div>
                 {!isFull && <div className="flex items-center justify-center flex-col">
                     <Logout />
                 </div>}
